38d40d68b20bca8bda66eb6276428b05c09314e4
amant555 <amant5186@gmail.com>
2021-12-05 22:31:25 +0530

    fix

--- Competitions/Rhetorical-Role/leaderboard/script.sh
+++
@@ -1,7 +0,0 @@
-source /root/.bashrc
-cd /root/Legal-NLP-EkStep.github.io
-git pull
-cp /root/secrets.json /root/Legal-NLP-EkStep.github.io/Competitions/Rhetorical-Role/leaderboard/
-rm -rf /root/Legal-NLP-EkStep.github.io/Competitions/Rhetorical-Role/leaderboard/out.json
-python /root/Legal-NLP-EkStep.github.io/Competitions/Rhetorical-Role/leaderboard/leaderboard_utils.py
-sudo shutdown
