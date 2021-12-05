465470e907cd5bab79cbf6c663ed88f3e7345add
amant555 <amant5186@gmail.com>
2021-12-05 21:00:47 +0530

    Add script.sh to automate shutdown and execution of generating leaderboard

---
+++ Competitions/Rhetorical-Role/leaderboard/script.sh
@@ -0,0 +1,5 @@
+source /home/aman_tiwari/.bashrc
+git pull
+rm -rf /home/aman_tiwari/Legal-NLP-EkStep.github.io/Competitions/Rhetorical-Role/leaderboard/out.json
+python /home/aman_tiwari/Legal-NLP-EkStep.github.io/Competitions/Rhetorical-Role/leaderboard/leaderboard_utils.py
+sudo shutdown
