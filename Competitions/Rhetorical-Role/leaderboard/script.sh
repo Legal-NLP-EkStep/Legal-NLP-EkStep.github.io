a9b34621beca4d3427fe090d1c59a928853e6411
amant555 <amant5186@gmail.com>
2021-12-05 21:47:01 +0530

    Changes for root

--- Competitions/Rhetorical-Role/leaderboard/script.sh
+++ Competitions/Rhetorical-Role/leaderboard/script.sh
@@ -1,5 +1,6 @@
-source /home/aman_tiwari/.bashrc
+source /root/.bashrc
+cd /root/Legal-NLP-EkStep.github.io
 git pull
-rm -rf /home/aman_tiwari/Legal-NLP-EkStep.github.io/Competitions/Rhetorical-Role/leaderboard/out.json
-python /home/aman_tiwari/Legal-NLP-EkStep.github.io/Competitions/Rhetorical-Role/leaderboard/leaderboard_utils.py
+rm -rf /root/Legal-NLP-EkStep.github.io/Competitions/Rhetorical-Role/leaderboard/out.json
+python /root/Legal-NLP-EkStep.github.io/Competitions/Rhetorical-Role/leaderboard/leaderboard_utils.py
 sudo shutdown
